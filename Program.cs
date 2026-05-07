0db9f7713affe5fa970927c9716adb704b40b699
PBL075 <pavelgorea75@gmail.com>
2026-05-07 16:50:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace inventory
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inventar = {"Pusca" , "Pistolet" , "Cutit" , "Heal"};
+            inventar [0] = "Pusca_lvl2";
+
+            for (int i = 0 ; i < inventar.Length ; i++)
+            {
+                Console.WriteLine("El : " + inventar[i]);
+            }
+            
+        }
+    }
+}
